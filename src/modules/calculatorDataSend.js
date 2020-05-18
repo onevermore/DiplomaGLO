5f824ca1747827632047677a238dc4dc6c477458
Viktoriya Kass <vikavika233@gmail.com>
2020-05-19 04:26:23 +0500

    fixed old smth

--- src/modules/calculatorDataSend.js
+++ src/modules/calculatorDataSend.js
@@ -1,7 +1,6 @@
-
 const calculatorDataSend = () => {
 
-    //тут повторяются переменные из calc, не знаю как вернуть из calc объект(
+    //тут повторяются переменные из calc, не знаю как вернуть из calc объект  ((((
     const accordCalc = document.getElementById('accordion'),
         switch1 = document.getElementById('myonoffswitch'),
         switch2 = document.getElementById('myonoffswitch-two'),
@@ -23,65 +22,57 @@ const calculatorDataSend = () => {
     const sendCalc = (event) => {
 
 
-        console.log('event: ', event);
-        const target = event.target;
-        console.log('target: ', target);
-
-        console.log(target.classList.contains('popup-close'));
-
-        if (!target.matches('.circle')) {
-            console.log(target.matches('button'));
-            event.preventDefault();
+        event.preventDefault();
 
 
-            //значения из селектов в калькуляторе(остальные переменные через value в самом объекте)
-            const d1val = diameter1.options[diameter1.selectedIndex].text,
-                r1val = ring1.options[ring1.selectedIndex].text,
+        //значения из селектов в калькуляторе(остальные переменные через value в самом объекте)
+        const d1val = diameter1.options[diameter1.selectedIndex].text,
+            r1val = ring1.options[ring1.selectedIndex].text,
 
-                d2val = diameter2.options[diameter2.selectedIndex].text,
-                r2val = ring2.options[ring2.selectedIndex].text;
+            d2val = diameter2.options[diameter2.selectedIndex].text,
+            r2val = ring2.options[ring2.selectedIndex].text;
 
-            //объект, который будем отправлять
-            const calcData = {
+        //объект, который будем отправлять
+        const calcData = {
 
-                camera: switch1.value,
-                diameter1: d1val,
-                rings1: r1val,
-                diameter2: d2val,
-                rings2: r2val,
-                bottom: switch2.value,
-                distance: distance.value,
-                calcResult: totalSum.value
-            };
+            camera: switch1.value,
+            diameter1: d1val,
+            rings1: r1val,
+            diameter2: d2val,
+            rings2: r2val,
+            bottom: switch2.value,
+            distance: distance.value,
+            calcResult: totalSum.value
+        };
 
 
-            const formData = new FormData(myCalcForm);
-            // в объект добавляем данные из формы
-            formData.forEach((val, key) => {
-                calcData[key] = val;
+        const formData = new FormData(myCalcForm);
+        // в объект добавляем данные из формы
+        formData.forEach((val, key) => {
+            calcData[key] = val;
+        });
+
+
+        //функция запроса на сервер
+        const postData = body => fetch('./server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+
+        });
+
+        postData(calcData)
+            .then(response => {
+                if (response.status !== 200) {
+                    throw new Error('status not 200');
+                }
+            })
+            .catch(error => {
+                console.error(error);
             });
 
-
-            //функция запроса на сервер
-            const postData = body => fetch('./server.php', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(body)
-
-            });
-
-            postData(calcData)
-                .then(response => {
-                    if (response.status !== 200) {
-                        throw new Error('status not 200');
-                    }
-                })
-                .catch(error => {
-                    console.error(error);
-                });
-        }
 
     };
 
